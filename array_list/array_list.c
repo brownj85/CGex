dd86da4f5fcbe0b62732e7fdcc2146e6e4139649
Jack <jb.brown@mail.utoronto.ca>
2019-03-10 15:36:38 -0400

    Implemented arrayList iterator

--- array_list/array_list.c
+++ array_list/array_list.c
@@ -5,13 +5,52 @@
 #include "../util.h"
 #include "array_list.h"
 
-arrayList arrayList_make(size_t elem_size){
+
+//  == Utility Functions ==
+
+static inline void *inc_by_bytes(void *ptr, size_t nbytes){
+    return (void *) (((char *) ptr) + nbytes);
+}
+
+static void arrayList_resize(arrayList *arr){
+  
+    arr->_elem = realloc(arr->_elem, (arr->_cap + ARRAYLIST_ITER) * arr->_elem_size);
+
+    memset(&arr->_elem[arr->_cap * arr->_elem_size], 0, ARRAYLIST_ITER * arr->_elem_size);
+
+    arr->_cap += ARRAYLIST_ITER;
+}
+
+//  ==
+
+//  == Iterator Functions ==
+
+void *aL_first(arrayList *arr){
+    return (void *) arr->_elem;
+}
+
+void *aL_next(arrayList *arr, void *iter){
+    return inc_by_bytes(iter, arr->_elem_size);
+}
+
+void *aL_done(arrayList *arr){
+    return inc_by_bytes(arr->_elem, arr->len * arr->_elem_size);
+}
+
+//  ==
+ 
+arrayList arrayList_make(size_t elem_size, bool alloc){
     arrayList arr;
 
-    arr._elem = calloc(ARRAYLIST_ITER * elem_size, 1);
-    arr._cap = ARRAYLIST_ITER;
+    if(alloc){
+        arr._elem = calloc(ARRAYLIST_ITER * elem_size, 1);
+        arr._cap = ARRAYLIST_ITER;
+    }else{
+        arr._elem = NULL;
+        arr._cap = 0;
+    }
+    
     arr._elem_size = elem_size;
-
     arr.len = 0;
 
     return arr;
@@ -31,36 +70,32 @@ void arrayList_get_cpy(arrayList *arr, size_t i, void *buf){
 
 void arrayList_set(arrayList *arr, size_t i, void *elem){
     if(i >= arr->len){
-        fprintf(stderr, "index error in arrayList: %ld\n", i);
+        fprintf(stderr, "indexError:  %ld\n", i);
         exit(1);
     }
 
     memcpy(&arr->_elem[i * arr->_elem_size], elem, arr->_elem_size);
 }
 
-static void arrayList_resize(arrayList *arr){
-  
-    arr->_elem = realloc(arr->_elem, (arr->_cap + ARRAYLIST_ITER) * arr->_elem_size);
 
-    memset(&arr->_elem[arr->_cap * arr->_elem_size], 0, ARRAYLIST_ITER * arr->_elem_size);
-
-    arr->_cap += ARRAYLIST_ITER;
-}
-
-void arrayList_append(arrayList *arr, void *elem){
-    arr->len++;
+size_t arrayList_append(arrayList *arr, void *elem){
+    arr->len = arr->len + 1;
 
     if(arr->len == arr->_cap)
         arrayList_resize(arr);
 
     arrayList_set(arr, arr->len - 1, elem);
+
+    return arr->len;
 }
 
-void arrayList_pop(arrayList *arr, void *buf){
+size_t arrayList_pop(arrayList *arr, void *buf){
     if(arr->len == 0){
-        fprintf(stderr, "popping from empty list");
+        fprintf(stderr, "indexError: pop from empty list\n");
         exit(1);
     }
     arr->len--;
     arrayList_get_cpy(arr, arr->len, buf);
+
+    return arr->len;
 }   
--- array_list/array_list.h
+++ array_list/array_list.h
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdbool.h>
 
 #ifndef ARRAYLIST_ITER
 #define ARRAYLIST_ITER 128
@@ -12,7 +13,8 @@ typedef struct arrayList{
     size_t _elem_size;
 }arrayList;
 
-arrayList arrayList_make(size_t elem_size);
+
+arrayList arrayList_make(size_t elem_size, bool alloc);
 
 void arrayList_free(arrayList *arr);
 
@@ -22,6 +24,12 @@ void arrayList_get_cpy(arrayList *arr, size_t i, void *buf);
 
 void arrayList_set(arrayList *arr, size_t i, void *elem);
 
-void arrayList_append(arrayList *arr, void *elem);
+size_t arrayList_append(arrayList *arr, void *elem);
 
-void arrayList_pop(arrayList *arr, void *buf);
+size_t arrayList_pop(arrayList *arr, void *buf);
+
+void *aL_first(arrayList *arr);
+
+void *aL_next(arrayList *arr, void *iter);
+
+void *aL_done(arrayList *arr);
